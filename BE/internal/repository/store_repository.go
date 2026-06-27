b88cc1efb413cbf9c76d9a9f5eec6ec6ad76cd27
Artt Vincii <otong123321@gmail.com>
2026-06-28 03:00:07 +0700

    feat: tambah method FindByUserID dan CountByName di StoreRepository

--- BE/internal/repository/store_repository.go
+++ BE/internal/repository/store_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/traa/seapedia/server/internal/entity"
+	"gorm.io/gorm"
 )
 
 type StoreRepository struct {
@@ -12,4 +13,14 @@ type StoreRepository struct {
 
 func NewStoreRepository(log *logrus.Logger) *StoreRepository {
 	return &StoreRepository{Log: log}
+}
+
+func (r *StoreRepository) FindByUserID(db *gorm.DB, store *entity.Store, userID string) error {
+	return db.Where("user_id = ?", userID).Take(store).Error
+}
+
+func (r *StoreRepository) CountByName(db *gorm.DB, name string) (int64, error) {
+	var count int64
+	err := db.Model(&entity.Store{}).Where("name = ?", name).Count(&count).Error
+	return count, err
 }
