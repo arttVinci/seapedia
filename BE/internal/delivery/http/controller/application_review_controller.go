89a874f961ccdcd09f8f18199780fb1c53168bde
Artt Vincii <otong123321@gmail.com>
2026-06-28 01:42:19 +0700

    feat: tambah controller list dan create application_review

---
+++ BE/internal/delivery/http/controller/application_review_controller.go
@@ -0,0 +1,52 @@
+package controller
+
+import (
+	"math"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+	"github.com/traa/seapedia/server/internal/model"
+	"github.com/traa/seapedia/server/internal/usecase"
+)
+
+type ApplicationReviewController struct {
+	UseCase *usecase.ApplicationReviewUseCase
+	Log     *logrus.Logger
+}
+
+func NewApplicationReviewController(useCase *usecase.ApplicationReviewUseCase, log *logrus.Logger) *ApplicationReviewController {
+	return &ApplicationReviewController{UseCase: useCase, Log: log}
+}
+
+func (c *ApplicationReviewController) List(ctx *fiber.Ctx) error {
+	request := &model.SearchApplicationReviewRequest{
+		Page: ctx.QueryInt("page", 1),
+		Size: ctx.QueryInt("size", 10),
+	}
+	responses, total, err := c.UseCase.Search(ctx.UserContext(), request)
+	if err != nil {
+		c.Log.Warnf("Failed to search reviews : %+v", err)
+		return err
+	}
+	paging := &model.PageMetadata{
+		Page:      request.Page,
+		Size:      request.Size,
+		TotalItem: total,
+		TotalPage: int64(math.Ceil(float64(total) / float64(request.Size))),
+	}
+	return ctx.JSON(model.WebResponse[[]model.ApplicationReviewResponse]{Data: responses, Paging: paging, Message: "Daftar ulasan", Success: true})
+}
+
+func (c *ApplicationReviewController) Create(ctx *fiber.Ctx) error {
+	request := new(model.CreateApplicationReviewRequest)
+	if err := ctx.BodyParser(request); err != nil {
+		c.Log.Warnf("Failed to parse request body : %+v", err)
+		return fiber.NewError(fiber.StatusBadRequest, "Format data request tidak valid")
+	}
+	response, err := c.UseCase.Create(ctx.UserContext(), request)
+	if err != nil {
+		c.Log.Warnf("Failed to create review : %+v", err)
+		return err
+	}
+	return ctx.Status(fiber.StatusCreated).JSON(model.WebResponse[*model.ApplicationReviewResponse]{Data: response, Message: "Ulasan berhasil dikirim", Success: true})
+}
