e930cee56de457fdaf2e6bef42dc416615e5ee3f
Artt Vincii <otong123321@gmail.com>
2026-06-28 18:04:19 +0700

    feat: add wallet and address types

--- FE/src/@types/models/index.ts
+++ FE/src/@types/models/index.ts
@@ -2,3 +2,5 @@ export * from './product.types';
 export * from './review.types';
 export * from './auth.types';
 export * from './store.types';
+export * from './wallet.types';
+export * from './address.types';
