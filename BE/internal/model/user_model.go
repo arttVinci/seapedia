14374df9dceb98eead1edb2bdcd619d0757da8bc
Artt Vincii <otong123321@gmail.com>
2026-06-28 13:08:01 +0700

    fix: switch login from username to email

--- BE/internal/model/user_model.go
+++ BE/internal/model/user_model.go
@@ -22,7 +22,7 @@ type RegisterUserResponse struct {
 }
 
 type LoginUserRequest struct {
-	Username string `json:"username" validate:"required"`
+	Email    string `json:"email" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
 
