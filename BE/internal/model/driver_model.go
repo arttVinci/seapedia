77fdc016d7bd6aadb18302402cfdc8211d546a7e
Artt Vincii <otong123321@gmail.com>
2026-06-28 22:47:30 +0700

    feat(driver): tambah struct model CompleteJobResponse dan DashboardResponse

--- BE/internal/model/driver_model.go
+++ BE/internal/model/driver_model.go
@@ -18,3 +18,31 @@ type JobResponse struct {
 type TakeJobResponse struct {
 	Success bool `json:"success"`
 }
+
+type CompleteJobResponse struct {
+	Success    bool  `json:"success"`
+	DriverEarning int64 `json:"driver_earning"`
+}
+
+type DashboardResponse struct {
+	ActiveJob      *DashboardActiveJob  `json:"active_job"`
+	CompletedToday int                  `json:"completed_today"`
+	TotalEarning   int64                `json:"total_earning"`
+	RecentJobs     []RecentJobResponse  `json:"recent_jobs"`
+}
+
+type DashboardActiveJob struct {
+	OrderID        string `json:"order_id"`
+	StoreID        string `json:"store_id"`
+	DeliveryMethod string `json:"delivery_method"`
+	DeliveryFee    int64  `json:"delivery_fee"`
+	TakenAt        *int64 `json:"taken_at"`
+}
+
+type RecentJobResponse struct {
+	OrderID       string `json:"order_id"`
+	Status        string `json:"status"`
+	DeliveryFee   int64  `json:"delivery_fee"`
+	DriverEarning int64  `json:"driver_earning"`
+	CompletedAt   *int64 `json:"completed_at"`
+}
