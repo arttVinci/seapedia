bdab01bafcf91168b7c5610d009579e7afed2fc0
Artt Vincii <otong123321@gmail.com>
2026-06-28 18:04:37 +0700

    feat: add wallet service

--- FE/src/services/index.ts
+++ FE/src/services/index.ts
@@ -1,5 +1,6 @@
 export { default as authService } from './authService';
 export { default as reviewService } from './reviewService';
+export { default as walletService } from './walletService';
 export * from './productService';
 export * from './sellerProductService';
 export * from './storeService';
