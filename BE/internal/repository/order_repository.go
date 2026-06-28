b6de60f4db4f40fc03d90149ee3b80dbe569cc7f
Artt Vincii <otong123321@gmail.com>
2026-06-28 15:59:56 +0700

    feat: add order query repository methods

--- BE/internal/repository/order_repository.go
+++ BE/internal/repository/order_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/traa/seapedia/server/internal/entity"
+	"gorm.io/gorm"
 )
 
 type OrderRepository struct {
@@ -12,4 +13,22 @@ type OrderRepository struct {
 
 func NewOrderRepository(log *logrus.Logger) *OrderRepository {
 	return &OrderRepository{Log: log}
+}
+
+func (r *OrderRepository) ListByBuyer(db *gorm.DB, buyerID string) ([]entity.Order, error) {
+	var orders []entity.Order
+	err := db.Where("buyer_id = ?", buyerID).Order("created_at desc").Find(&orders).Error
+	if err != nil {
+		return nil, err
+	}
+	return orders, nil
+}
+
+func (r *OrderRepository) FindByBuyerAndID(db *gorm.DB, buyerID string, orderID string) (*entity.Order, error) {
+	var order entity.Order
+	err := db.Preload("Items").Preload("StatusHistories").Where("buyer_id = ? AND id = ?", buyerID, orderID).Take(&order).Error
+	if err != nil {
+		return nil, err
+	}
+	return &order, nil
 }
