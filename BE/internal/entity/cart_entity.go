b352abb21c90d0e37363a8221477de2d88e22be4
Artt Vincii <otong123321@gmail.com>
2026-06-28 15:27:23 +0700

    feat: add migration and entity for carts

---
+++ BE/internal/entity/cart_entity.go
@@ -0,0 +1,17 @@
+package entity
+
+type Cart struct {
+	ID        string  `gorm:"primaryKey;column:id"`
+	UserID    string  `gorm:"column:user_id"`
+	StoreID   *string `gorm:"column:store_id"` // nullable
+	CreatedAt int64   `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt int64   `gorm:"column:updated_at;autoUpdateTime"`
+
+	// Relations (if needed)
+	User  *User  `gorm:"foreignKey:UserID"`
+	Store *Store `gorm:"foreignKey:StoreID"`
+}
+
+func (c *Cart) TableName() string {
+	return "carts"
+}
