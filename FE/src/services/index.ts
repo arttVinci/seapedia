1354b588b0baa67f735eea3bf0738381994ab807
Artt Vincii <otong123321@gmail.com>
2026-06-28 18:04:57 +0700

    feat: add address service

--- FE/src/services/index.ts
+++ FE/src/services/index.ts
@@ -1,6 +1,7 @@
 export { default as authService } from './authService';
 export { default as reviewService } from './reviewService';
 export { default as walletService } from './walletService';
+export { default as addressService } from './addressService';
 export * from './productService';
 export * from './sellerProductService';
 export * from './storeService';
